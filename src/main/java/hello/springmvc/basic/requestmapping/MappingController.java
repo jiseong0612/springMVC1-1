1d9088563af4f46229cc714a222079632c0a49c3
jiseong0612 <hth5545@gmail.com>
2022-03-08 18:23:05 +0900

    스프링 api 파라미터 매핑 방법

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -4,6 +4,7 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,5 +29,19 @@ public class MappingController {
 	public String  mappingPath(@PathVariable String userId, @PathVariable String orderId) {
 		log.info("data = {}, orderId = {}", userId, orderId);
 		return userId + ", "+ orderId;
+	}
+	
+	//서버가 소비하는 입장이다. 따라서 consumes
+	@PostMapping(value = "/mapping-consume", consumes = "application/json", produces = "text/pain")
+	public String mappingConsume() {
+		log.info("mappingConsume");
+		return "OK~!";
+	}
+	
+	//서버가 리턴해주는 즉 생산하는 값의 타입 produces, 헤더의 Accept를 맞춰줘야함
+	@PostMapping(value = "/mapping-produces", produces = "text/pain")
+	public String mappingProduces() {
+		log.info("mappingProduces");
+		return "OK~!";
 	}
 }

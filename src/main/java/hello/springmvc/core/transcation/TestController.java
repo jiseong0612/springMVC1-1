ce8040f8114125e1f5e89d55ea3864a744de63ed
jiseong0612 <hth5545@gmail.com>
2022-03-24 14:18:04 +0900

    예제 commit

---
+++ src/main/java/hello/springmvc/core/transcation/TestController.java
@@ -0,0 +1,10 @@
+package hello.springmvc.core.transcation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+
+@Controller
+@Transactional
+public class TestController {
+
+}
---
+++ src/main/java/hello/springmvc/core/transcation/TestService.java
@@ -0,0 +1,10 @@
+package hello.springmvc.core.transcation;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class TestService {
+
+}

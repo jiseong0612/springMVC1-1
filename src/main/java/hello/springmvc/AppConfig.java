bd745b751c562672d88db877d515b0507c5d2eb9
jiseong0612 <hth5545@gmail.com>
2022-03-25 00:47:54 +0900

    d

--- src/main/java/hello/springmvc/AppConfig.java
+++ src/main/java/hello/springmvc/AppConfig.java
@@ -1,45 +1,28 @@
 package hello.springmvc;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import hello.springmvc.core.discount.DiscountPolicy;
-import hello.springmvc.core.discount.FixDiscountPolicy;
-import hello.springmvc.core.discount.RateDiscountPolicy;
-import hello.springmvc.core.repository.MemberRepository;
-import hello.springmvc.core.repository.MemoryMemberRepository;
-import hello.springmvc.core.service.MemberService;
-import hello.springmvc.core.service.MemberServiceImpl;
-import hello.springmvc.core.service.OrderService;
-import hello.springmvc.core.service.OrderServiceImpl;
-
-@Configuration
+//@Configuration
 public class AppConfig {
 
-	// 새로운 메서드를 만듦으로써 중복제거
-	@Bean
-	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
-	}
-
-	@Bean
-	public DiscountPolicy fixDiscountPolicy() {
-		return new FixDiscountPolicy();
-	}
-
-	@Bean
-	public DiscountPolicy rateDiscountPolicy() {
-		return new RateDiscountPolicy();
-	}
-
-	@Bean
-	public MemberService memberService() {
-		return new MemberServiceImpl(memberRepository());
-	}
-
-	@Bean
-	public OrderService orderService() {
-		return new OrderServiceImpl(memberRepository(), fixDiscountPolicy());
-	}
+//	// 새로운 메서드를 만듦으로써 중복제거
+//	@Bean
+//	public MemberRepository memberRepository() {
+//		return new MemoryMemberRepository();
+//	}
+//
+//	@Bean
+//	public DiscountPolicy discountPolicy() {
+//		return new FixDiscountPolicy();
+//		// new RateDiscountPolicy();
+//	}
+//
+//	@Bean
+//	public MemberService memberService() {
+//		return new MemberServiceImpl(memberRepository());
+//	}
+//
+//	@Bean
+//	public OrderService orderService() {
+//		return new OrderServiceImpl(memberRepository(), discountPolicy());
+//	}
 
 }
--- src/main/java/hello/springmvc/core/service/OrderServiceImpl.java
+++ src/main/java/hello/springmvc/core/service/OrderServiceImpl.java
@@ -1,17 +1,19 @@
 package hello.springmvc.core.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import hello.springmvc.core.discount.DiscountPolicy;
 import hello.springmvc.core.domain.Member;
 import hello.springmvc.core.domain.Order;
 import hello.springmvc.core.repository.MemberRepository;
-@Component("service")
-public class OrderServiceImpl implements OrderService{
-	
+
+@Component
+public class OrderServiceImpl implements OrderService {
 	private final MemberRepository memberRepository;
-	private  final DiscountPolicy discountPolicy;
-	
+	private final DiscountPolicy discountPolicy;
+
+	@Autowired
 	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
@@ -22,8 +24,7 @@ public class OrderServiceImpl implements OrderService{
 		Member member = memberRepository.findById(memberId);
 		int discountPrice = discountPolicy.discount(member, itemPrice);
 		// 단일 책임 법칙을 잘 지킨것이.... 할인금액? createOrder() ="난 몰라?, 'discountPolicy' 니가 알아서 해줘"
-		
+
 		return new Order(memberId, itemName, itemPrice, discountPrice);
 	}
-
 }
